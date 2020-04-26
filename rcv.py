82246af55614ff0e277a4628ef74e4935f1168e4
fegan104 <fegan104@gmail.com>
2020-04-25 23:36:25 -0400

    added tie breaker for elimination

--- rcv.py
+++ rcv.py
@@ -38,31 +38,31 @@ class Candidate:
         surplus_per_vote = surplus / len(self.__ballots)
         votes_for_candidate = sum(
             [1 for b in self.__ballots if len(b) > 0 and b[0] == candidate_name])
-        result = round(votes_for_candidate * surplus_per_vote)
+        result = math.floor(votes_for_candidate * surplus_per_vote)
         self.__votes -= result
         return result
 
     def exhausted_ballots(self, surplus):
         surplus_per_vote = surplus / len(self.__ballots)
         exhausted_votes = sum([1 for b in self.__ballots if len(b) == 0])
-        result = round(exhausted_votes * surplus_per_vote)
+        result = math.floor(exhausted_votes * surplus_per_vote)
         self.__votes -= result
         return result
 
     def add_surplus(self, surplus):
-        self.__votes += round(surplus)
+        self.__votes += math.floor(surplus)
 
     def surplus(self, quota):
         return max(self.__votes - quota, 0.0)
 
-    def add_ballots(self, new_ballots):
-        self.__ballots.append(new_ballots)
-        self.__votes += len(new_ballots)
+    def add_ballot(self, new_ballot):
+        self.__ballots.append(new_ballot)
+        self.__votes += 10_000  # add a vote for the corresponding ballot
 
     def drop_candidate(self, candidate):
         for b in self.__ballots:
-            if(candidate in b):
-                b.remove(candidate)
+            if(candidate.name in b):
+                b.remove(candidate.name)
 
     def __repr__(self):
         return str(self)
@@ -72,6 +72,9 @@ class Candidate:
 
     def __lt__(self, other):
         return self.votes < other.votes
+
+    def __eq__(self, other):
+        return other is Candidate and self.name == other.name
 
 
 def award_first_pref(candidate_names, ballot_data):
@@ -113,24 +116,55 @@ def distribute_surplus(candidates, exhausted, quota):
     return candidates
 
 
-def redisitribute_loser(candidates, exhausted):
+def redisitribute_loser(candidates, exhausted, round_results):
     """ 
         Returns: A list of Candidates with the lowest vote
             getting Canidate removed
     """
-    biggest_loser = min(candidates)
-    candidates.remove(biggest_loser)
+    biggest_losers = [
+        c for c in candidates if c.votes == min(candidates).votes]
+    eliminated = pick_loser(biggest_losers, round_results)
+    candidates.remove(eliminated)
 
-    for b in biggest_loser.ballots:
+    for b in eliminated.ballots:
         next_choice = b[0] if len(b) > 0 else None
         if(next_choice == None):
-            exhausted.add_ballots(b)
+            exhausted.add_ballot(b)
         for c in candidates:
-            c.drop_candidate(biggest_loser)
+            c.drop_candidate(eliminated)
             if c.name == next_choice:
-                c.add_ballots(b)
+                c.add_ballot(b[1:])
 
     return candidates
+
+
+def pick_loser(tied, round_results):
+    """ Breaks the tie by recursively picking the candidate with the most 
+        votes in the last round.
+
+    Args:
+        tied: List[Candidate]
+        round_results: List of Tuples of candidate name to votes where each
+                       list item is the reults from that round.
+
+    Returns: The candidate that will be redistributed.
+    """
+    if len(tied) == 1:
+        return tied[0]  # There is no tie return sole loser
+    if not round_results:
+        return tied[0]  # We can't break any remaing ties just pick a loser
+    tied_names = [c.name for c in tied]
+    rnd = round_results[-1]
+    min_votes = min([c for c in rnd if c[0] in tied_names],
+                    key=lambda r: r[1])[1]
+    loser_names = [c[0]
+                   for c in rnd if c[0] in tied_names and c[1] == min_votes]
+    loser_candidates = [c for c in tied if c.name in loser_names]
+
+    if(len(loser_candidates) == 1):
+        return loser_candidates[0]
+
+    return pick_loser(loser_candidates, round_results[:-1])
 
 
 def parse_vote_data(csv_data):
@@ -170,38 +204,41 @@ def count_ballots(file_name, num_winners):
         candidate_names, vote_data = parse_vote_data(csv_data)
         candidates_to_votes = award_first_pref(candidate_names, vote_data)
         winners = {}
-        round_num = 1
+        round_results = []
         exhausted = Candidate("Exhausted", [])
 
-
-        while len(winners.keys()) < num_winners:
+        while True:
             num_votes = sum([c.votes for c in candidates_to_votes])
             quota = calculate_quota(num_winners, num_votes)
             candidates_to_votes.sort(reverse=True)
-            #Print stats
-            print(f"Round {round_num}:")
+            # Print stats
+            print(f"Round {len(round_results) + 1}:")
             print(f"Quota to win: {quota}")
             for c in candidates_to_votes:
                 print(f"  {c.name}: {c.votes}")
 
-            #Update winners
+            round_results.append([(c.name, c.votes)
+                                  for c in candidates_to_votes])
+
+            # Update winners
+            winners.clear()
             for w in find_winners(candidates_to_votes, quota):
                 winners[w.name] = w
 
-            if(len(winners) >= num_winners):
+            if(len(winners) == num_winners):
                 return winners, exhausted
             elif(sum([w.surplus(quota) for w in winners.values()]) > 0.0):
-                candidates_to_votes = distribute_surplus(candidates_to_votes, 
-                    exhausted, quota)
+                candidates_to_votes = distribute_surplus(
+                    candidates_to_votes, exhausted, quota)
             else:
                 candidates_to_votes = redisitribute_loser(
-                    candidates_to_votes, exhausted)
-            round_num += 1
+                    candidates_to_votes, exhausted, round_results)
+            
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Administer election.')
-    parser.add_argument('-f', '--file', action='store', type=str, default="ice_cream2.csv",
+    parser.add_argument('-f', '--file', action='store', type=str,
                         help='Name of the election results file')
     parser.add_argument('-n', '--num_winners', action='store',
                         type=int, default=1, help='Number of winners to pick')
--- rcv_test.py
+++ rcv_test.py
@@ -1,23 +1,24 @@
-import rcv
+from rcv import * 
 import unittest
 from test import support
+import csv
 
 class ElectionTest(unittest.TestCase):
 
     def test_quota(self):
-        self.assertTrue(rcv.calculate_quota(num_winners=1, num_votes=10) == 5)
-        self.assertTrue(rcv.calculate_quota(num_winners=1, num_votes=100) == 50)
-        self.assertTrue(rcv.calculate_quota(num_winners=3, num_votes=100) == 25)
+        self.assertTrue(calculate_quota(num_winners=1, num_votes=10) == 5)
+        self.assertTrue(calculate_quota(num_winners=1, num_votes=100) == 50)
+        self.assertTrue(calculate_quota(num_winners=3, num_votes=100) == 25)
 
     def test_abc_1(self):
-        winners, exhausted = rcv.count_ballots("results/abc.csv", num_winners=1)
+        winners, exhausted = count_ballots("results/abc.csv", num_winners=1)
         self.assertTrue(len(winners.values()) == 1)
         self.assertTrue(winners["Alice"].name == "Alice")
         self.assertTrue(winners["Alice"].votes == 60_000)
         self.assertTrue(exhausted.votes == 0)
 
     def test_abc_2(self):
-        winners, exhausted = rcv.count_ballots("results/abc.csv", num_winners=2)
+        winners, exhausted = count_ballots("results/abc.csv", num_winners=2)
         self.assertTrue(len(winners.values()) == 2)
         self.assertTrue(winners["Alice"].name == "Alice")
         self.assertTrue(winners["Bill"].name == "Bill")
@@ -26,10 +27,38 @@ class ElectionTest(unittest.TestCase):
         self.assertTrue(exhausted.votes == 30_000)
 
     def test_bookclub(self):
-        winners, exhausted = rcv.count_ballots("results/book_club_responses.csv", num_winners=1)
+        winners, exhausted = count_ballots("results/book_club_responses.csv", num_winners=1)
         self.assertTrue(len(winners.values()) == 1)
         self.assertTrue(max(winners.values()).name == "[One Day]")
         self.assertTrue(exhausted.votes == 0)
+
+    def test_elections(self):
+        winners, exhausted = count_ballots("results/elections.csv", num_winners=1)
+        self.assertTrue(len(winners.values()) == 1)
+        self.assertTrue(max(winners.values()).votes == 80_000)
+        self.assertTrue(exhausted.votes == 0)
+
+    def test_pick_loser(self):
+        with open("results/tie.csv") as csv_file:
+            num_winners = 1
+            csv_data = csv.reader(csv_file, delimiter=',')
+            candidate_names, vote_data = parse_vote_data(csv_data)
+            candidates = award_first_pref(candidate_names, vote_data)
+            round_results = [[(c.name, c.votes) for c in candidates]]
+            num_votes = sum([c.votes for c in candidates])
+            exhausted = Candidate("Exhausted", [])
+            quota = calculate_quota(num_winners, num_votes)
+            tied = [Candidate("Alice", ["Alice"]), Candidate("Bill", ["Alice", "Alice", "Alice", "Alice"])]
+            result = pick_loser(tied, round_results)
+            self.assertTrue(type(result) is Candidate)
+            self.assertTrue(result.name == "Bill")
+            self.assertTrue(result.votes == 40_000)
+
+    def test_tie(self):
+        winners, exhausted = count_ballots("results/tie.csv", num_winners=1)
+        self.assertEqual(1, len(winners))
+        self.assertEqual(60_000, max(winners.values()).votes)
+        self.assertEqual(60_000, exhausted.votes)
 
 if __name__ == '__main__':
     unittest.main()
